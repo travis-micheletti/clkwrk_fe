1e06a9a3b31255990e085b64d4a0e77ec3e35c42
Travis Micheletti <106038655+travis-micheletti@users.noreply.github.com>
2022-08-24 12:39:08 -0700

    adding links to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 // Nav Bar - > displays on all pages from the home page and on 
+import { Link } from 'react-router-dom'
 import UserSettings from './UserSettings'
 import { motion } from 'framer-motion'
 
@@ -6,6 +7,11 @@ const NavBar = () => {
 	return (
 		<motion.div>
 			<UserSettings />
+			<Link to='/'>home</Link>
+			<Link to='/employees'>employees</Link>
+			<Link to='/welcome'>welcome</Link>
+			<Link to='/profile'>profile</Link>
+			<Link to='/logged-out'>logout</Link>
 			navbar
 		</motion.div>
 	)
